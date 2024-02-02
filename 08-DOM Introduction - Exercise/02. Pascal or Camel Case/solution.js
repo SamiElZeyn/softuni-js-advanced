5ee752a66ce2792b4e9beba85ba7a9f99816c72b
SamiElZeyn <samielzeyn@gmail.com>
2024-02-02 13:58:25 +0200

    adding new content

---
+++ 08-DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,27 @@
+function solve() {
+  const resultRef = document.getElementById('result');
+  let textValue = document.getElementById('text').value;
+  let convention = document.getElementById("naming-convention").value;
+  textValue = textValue.toLowerCase();
+  let textArr = textValue.split(" ");
+  let result = '';
+
+  switch (convention) {
+    case "Camel Case":
+
+      result = textArr.shift();
+      textArr.forEach(word => {
+        result += word[0].toUpperCase() + word.substring(1).toLowerCase();
+      });
+      break;
+    case "Pascal Case":
+      textValue.forEach(word => {
+        result += word[0].toUpperCase() + word.substring(1);
+      });
+      break;
+    default: result = "Error!";
+      break;
+  }
+
+  resultRef.textContent = result;
+}
