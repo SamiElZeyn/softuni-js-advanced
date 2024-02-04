6080919799555657d60cd2b1c3c231621d5634b0
SamiElZeyn <samielzeyn@gmail.com>
2024-02-04 08:42:35 +0200

    adding new content

--- 08-DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 08-DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    
+    let butonName = document.getElementsByClassName(`button`)[0];
+    let divHiden = document.getElementById(`extra`);
+
+    if (butonName.textContent == `More`) {
+        divHiden.style.display = `block`;
+        butonName.textContent = `Less`;
+    } else if (butonName.textContent == `Less`) {
+
+        divHiden.style.display = `none`;
+        butonName.textContent = `More`;
+    }
+    debugger
 }
--- 08-DOM Introduction - Exercise/04. Search in List/search.js
+++ 08-DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   const searchField = document.getElementById('searchText');
+   const list = document.getElementById('towns');
+   const towns = Array.from(list.children).map(townEl => townEl.textContent);
+   let matches = 0;
+   
+   let resultDiv = document.querySelector('div#result');
+   
+   let searchText = searchField.value.toLowerCase();
+
+   towns.forEach(townLiElement => {
+      let townName = townLiElement.toLowerCase();
+      if (searchText && townName.includes(searchText)) {
+         townLiElement.innerHTML = `<bold><u>${townName}</u></bold>`;
+         matches++;
+      }
+   });
+   resultDiv.textContent = `${matches} matches found`;
+   debugger
 }
